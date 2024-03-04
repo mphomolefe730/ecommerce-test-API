567f1e2d4c080518c9c0b07fda2972dfe9fb4c98
Mpho Molefe <117911388+mphomolefe730@users.noreply.github.com>
2024-03-04 11:16:21 +0200

    Update environments.js
    Add render api link to environment file

--- environments.js
+++ environments.js
@@ -2,5 +2,6 @@ export const environment ={
     mongodb: {
         username: 'mphomolefe730',
         password:'3rFIkjvYKMsmD7Nz'
-    }
+    },
+    renderApiLink: 'https://ecommerce-test-api-i2se.onrender.com'
 }
