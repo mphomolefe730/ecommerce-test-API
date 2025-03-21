3cc70c82a348d8fce551e0d2d9e14710a749ea75
mphomolefe730 <mphomolefe730@gmail.com>
2025-03-21 12:06:12 +0200

    add recommendation end point and filter to get recommendations based on category relation

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,5 +1,6 @@
 import Express from "express";
 import { productModel } from '../models/product.model.js';
+import { homeManagementService } from '../services/home-management.service.js'
 import { Auth } from '../middleware/auth.js';
 
 export const productinkConnect = Express.Router();
@@ -20,7 +21,6 @@ productinkConnect.post('/add', async (req,res)=>{
     }
 }) 
 
-
 // ##############################################
 // ############## GET PRODUCT ###################
 // ##############################################
@@ -40,6 +40,59 @@ productinkConnect.get('/:id',async (req,res)=>{
         console.error(error);
         const { id } = req.params;
         return res.status(500).json(`failed to get product with id(${id})`)        
+    }
+})
+
+productinkConnect.get('/recommendations/:id/:userId/:page',async (req,res)=>{
+    try {
+        const { id, userId, page } = req.params;
+        let recommendationBasedOnCategory = [];
+        const product = await productModel.findById(id).populate([
+            {
+                path:"seller",
+                select:"name"
+            },
+            "categories"
+        ]);
+        
+        if (!product) return res.status(404).json(`product with id(${id}) not found`)
+
+        //get recommendation based on category
+        /*product.categories.forEach(async (c)=>{
+            const arrayOfProducts = await getProductRecommendations(c._id.toString(), userId, page);
+            arrayOfProducts.forEach((item)=>{
+               recommendationBasedOnCategory.push(item);
+               console.log("length: " + recommendationBasedOnCategory.length);
+            });
+        });*/
+        for(let i = 0; i<product.categories.length;i++){
+            const arrayOfProducts = await getProductRecommendations(product.categories[i]._id.toString(), userId, page);
+            arrayOfProducts.forEach((item)=>{
+               recommendationBasedOnCategory.push(item);
+            });
+        }
+        //removing of dulplicate enetities
+        let uniqueArray = recommendationBasedOnCategory.filter((item, index, self) =>
+                self.findIndex(obj => obj.id === item.id) === index
+        );
+        uniqueArray = uniqueArray.filter((p)=> p._id != id);
+        //send recommendation if we know the user else default recommendation
+        if (userId != 'undefined'){
+            return res.status(200).json({
+                "productDetails": product,
+                "recommendations": uniqueArray
+            });
+        }else{
+            return res.status(200).json({
+                "productDetails": product,
+                "recommendations": uniqueArray
+            });
+        }
+
+    } catch (error) {
+        console.error(error);
+        const { id } = req.params;
+        return res.status(500).json(`failed to get category with id(${id})`)        
     }
 })
 
@@ -128,3 +181,21 @@ productinkConnect.put('/:id' ,async (req,res)=>{
         });       
     }
 })
+
+
+// #################################################
+// ########## FUNCTION FOR RECOMMENDATIONS #########
+// #################################################
+let getProductRecommendations = async(categoryId, userId='undefinded', page)=> {
+    try{
+        let req = {
+           'body':{
+                'page': page
+            }
+        };
+        let homeManagementServiceManager = new homeManagementService();
+        return await homeManagementServiceManager.getByCategory(categoryId,req);
+    } catch (error){
+        console.log(error);
+    }
+}
